40587175c4175b838d676b6b4f22a21f74d74931
Ezana Mussie <ezuemg11@gmail.com>
2024-06-26 12:46:26 +0200

    Prompt changed

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,10 @@
-ask = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-if ask=="sunny":
-    print("Wear a t-shirt and sunglasses.")
-elif ask=="rainy":
+if weather=="sunny":
+    print("Wear a t-shirt and sunglasses.") 
+elif weather=="rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif ask=="cold":
+elif weather=="cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
-    
